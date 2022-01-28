49390282d2efcc98ab06ed4726d0bb96922b32a6
alpaunovski <75141862+alpaunovski@users.noreply.github.com>
2022-01-28 13:28:15 +0200

    finished app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function App() {
-  return <div />;
+
+//Destructuring the useState hook
+const [count, setCount] = useState(0);
+
+//Incrementing the count
+function increase () {
+  setCount(count + 1);
 }
 
+//Decrementing the count
+function decrease () {
+  setCount(count - 1);
+}
+
+return (
+
+<div className="container">
+
+<h1>{count}</h1>
+<button onClick={decrease}>-</button>
+<button onClick={increase}>+</button>
+
+</div>
+);
+
+}
 export default App;
